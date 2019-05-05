0232b3b01d6a16e194ff2c36773522a6829f0b9a
Suhendi <suhendi999@gmail.com>
2019-05-05 20:07:41 +0700

    Removed unused file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.css';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
-import Credits from './containers/Credits';
 import Main from './containers/Main';
 
 function App() {
   return (
     <Router className="App">
       <Route path="/" component={Main}/>
-      <Route path="/credits" component={Credits}/>
     </Router>
   );
 }
